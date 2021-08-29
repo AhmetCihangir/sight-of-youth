52c8f23d45ddde0eb0d9f4910e98b4f4c4be006c
AhmetCihangir <79200784+AhmetCihangir@users.noreply.github.com>
2021-08-29 20:27:18 +0300

    Update OurTeam.js

--- src/components/OurTeam/OurTeam.js
+++ src/components/OurTeam/OurTeam.js
@@ -6,16 +6,16 @@ const proje = "Proje", tasarim = "Tasarım", sosyalMedya = "Sosyal Medya", halkI
 
 const team = [
     {team : proje, members : [
-      "Öykü Tabak","Ece Eregez","Berna Özcan", "Meriç Uçarlar","Minel Başarır","Elif Yumuşakhuylu","Ravza Su Özer","Mine Özdemir",
+      { name : "Öykü Tabak", src : "/pp/Oyku.jpg" },{ name : "Ece Eregez", src : "/pp/Ece.jpg" },{ name : "Berna Özcan", src : "/pp/Berna.jpg" },{ name : "Meriç Uçarlar", src : "/pp/Meric.jpg" },{ name : "Minel Başarır", src : "/pp/Minel.jpg" },{ name : "Elif Yumuşakhuylu", src : "Elif Yumuşakhuylu" },{ name : "Mine Özdemir", src : "Mine Özdemir" }
     ]},
     {team : tasarim, members : [
-        "Beyza Özdemir","Zehra Aygür","Sinem Ünlü","Yusuf Çuhadar","A.Cihangir Gündoğdu"
+      { name : "Beyza Özdemir", src : "/pp/Beyza.jpg" },{ name : "Zehra Aygür", src : "/pp/Zehra.jpg" },{ name : "Sinem Ünlü", src : "/pp/Sinem.jpg" },{ name : "Yusuf Çuhadar", src : "Yusuf Çuhadar" },{ name : "A.Cihangir Gündoğdu", src : "/pp/Ahmet.jpg" }
     ]},
     {team : sosyalMedya, members : [
-        "Azra Aydemir","Beyza Özdemir","Pınar Gündüz","Uygar Çatal"
+      { name : "Azra Aydemir", src : "/pp/Azra.png" },{ name : "Beyza Özdemir", src : "/pp/Beyza.jpg" },{ name : "Pınar Gündüz", src : "/pp/Pinar.jpg" },{ name : "Uygar Çatal", src : "/pp/Uygar.jpg" }
     ]},
     {team : halkIleIlişkiler, members : [
-      "Zeynep Acar","Aylin Karabulut","Duru Gülsüm","Eylül Özten","Meliha Aydan Yücel","Melis Keskin","Nehir Nida Arslan"
+      { name : "Zeynep Acar", src : "/pp/Zeynep.jpg" },{ name : "Aylin Karabulut", src : "/pp/Aylin.jpg" },{ name : "Duru Gülsüm", src : "/pp/Duru.jpg" },{ name : "Eylül Özten", src : "/pp/Eylul.jpg" },{ name : "Meliha Aydan Yücel", src : "/pp/Meliha.jpg" },{ name : "Melis Keskin", src : "Melis Keskin" },{ name : "Nehir Nida Arslan", src : "/pp/Nehir.jpg" }
     ]},
 ]
 
