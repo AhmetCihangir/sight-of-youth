95ceb2b9b117dbf697975c92cc97b3c9e36cf45b
AhmetCihangir <79200784+AhmetCihangir@users.noreply.github.com>
2021-09-29 21:08:28 +0300

    Update OurTeam.js

--- src/components/OurTeam/OurTeam.js
+++ src/components/OurTeam/OurTeam.js
@@ -6,16 +6,16 @@ const proje = "Proje", tasarim = "Tasarım", sosyalMedya = "Sosyal Medya", halkI
 
 const team = [
     {team : proje, members : [
-      { name : "Öykü Tabak", src : "/pp/Oyku.jpg" },{ name : "Ece Eregez", src : "/pp/Ece.jpg" },{ name : "Berna Özcan", src : "/pp/Berna.jpg" },{ name : "Meriç Uçarlar", src : "/pp/Meric.jpg" },{ name : "Minel Başarır", src : "/pp/Minel.jpg" },{ name : "Elif Yumuşakhuylu", src : "Elif Yumuşakhuylu" },{ name : "Mine Özdemir", src : "Mine Özdemir" }
+      { name : "Öykü Tabak", src : "/pp/Oyku.jpg", manager : true },{ name : "Ece Eregez", src : "/pp/Ece.jpg", manager : true },{ name : "Berna Özcan", src : "/pp/Berna.jpg", manager : false },{ name : "Meriç Uçarlar", src : "/pp/Meric.jpg", manager : false },{ name : "Minel Başarır", src : "/pp/Minel.jpg", manager : false },{ name : "Elif Yumuşakhuylu", src : "Elif Yumuşakhuylu", manager : false },{ name : "Mine Özdemir", src : "Mine Özdemir", manager : false }
     ]},
     {team : tasarim, members : [
-      { name : "Beyza Özdemir", src : "/pp/Beyza.jpg" },{ name : "Zehra Aygür", src : "/pp/Zehra.jpg" },{ name : "Sinem Ünlü", src : "/pp/Sinem.jpg" },{ name : "Yusuf Çuhadar", src : "Yusuf Çuhadar" },{ name : "A.Cihangir Gündoğdu", src : "/pp/Ahmet.jpg" }
+      { name : "Beyza Özdemir", src : "/pp/Beyza.jpg", manager : false },{ name : "Zehra Aygür", src : "/pp/Zehra.jpg", manager : false },{ name : "Sinem Ünlü", src : "/pp/Sinem.jpg", manager : false },{ name : "Yusuf Çuhadar", src : "Yusuf Çuhadar", manager : false },{ name : "A.Cihangir Gündoğdu", src : "/pp/Ahmet.jpg", manager : false }
     ]},
     {team : sosyalMedya, members : [
-      { name : "Azra Aydemir", src : "/pp/Azra.png" },{ name : "Uygar Çatal", src : "/pp/Uygar.jpg" },{ name : "Beyza Özdemir", src : "/pp/Beyza.jpg" },{ name : "Pınar Gündüz", src : "/pp/Pinar.jpg" }
+      { name : "Azra Aydemir", src : "/pp/Azra.png", manager : true },{ name : "Uygar Çatal", src : "/pp/Uygar.jpg", manager : true },{ name : "Beyza Özdemir", src : "/pp/Beyza.jpg", manager : false },{ name : "Pınar Gündüz", src : "/pp/Pinar.jpg", manager : false }
     ]},
     {team : halkIleIlişkiler, members : [
-      { name : "Zeynep Acar", src : "/pp/Zeynep.jpg" },{ name : "Aylin Karabulut", src : "/pp/Aylin.jpg" },{ name : "Duru Gülsüm", src : "/pp/Duru.jpg" },{ name : "Eylül Özten", src : "/pp/Eylul.jpg" },{ name : "Meliha Aydan Yücel", src : "/pp/Meliha.jpg" },{ name : "Melis Keskin", src : "Melis Keskin" },{ name : "Nehir Nida Arslan", src : "/pp/Nehir.jpg" }
+      { name : "Zeynep Acar", src : "/pp/Zeynep.jpg", manager : true },{ name : "Aylin Karabulut", src : "/pp/Aylin.jpg", manager : false },{ name : "Duru Gülsüm", src : "/pp/Duru.jpg", manager : false },{ name : "Eylül Özten", src : "/pp/Eylul.jpg", manager : false },{ name : "Meliha Aydan Yücel", src : "/pp/Meliha.jpg", manager : false },{ name : "Melis Keskin", src : "Melis Keskin", manager : false },{ name : "Nehir Nida Arslan", src : "/pp/Nehir.jpg", manager : false }
     ]},
 ]
 
